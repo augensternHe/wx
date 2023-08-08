be777600b1bffc1db34837f3c9d7dc979d77e2ef
augensternHe <civihw1021@gmail.com>
2023-08-08 15:13:00 +0800

    the new branch

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 # 创建应用实例
 import sys
+from flask import Flask
+from flask import jsonify
 
 from wxcloudrun import app
+
+app = Flask(__name__)
+
+@app.route('/jiance', methods=['GET', 'POST'])
+def jiance():
+    return jsonify(1)
+
 
 # 启动Flask Web服务
 if __name__ == '__main__':
